591eeb95c1b39c4cd740959e9d62ee4e6f6eb3aa
crishelpc <202280108@psu.palawan.edu.ph>
2023-12-13 16:40:20 +0800

    add, edit, delete trainor

--- cardquest/urls.py
+++ cardquest/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import HomePageView, TrainerList
+from .views import HomePageView, TrainerList, TrainerCreateView, TrainerUpdateView, TrainerDeleteView
+from cardquest import views
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('trainer_list', TrainerList.as_view(), name='trainer_list'),
     path('pokemon-card', TrainerList.as_view(), name='pokemon-card'),
     path('collection', TrainerList.as_view(), name='collection'),
+    path('trainer_list/add', TrainerCreateView.as_view(), name='trainer-add'), 
+    path('trainer_list/<pk>', TrainerUpdateView.as_view(), name='trainer-update'),
+    path('trainer_list/<pk>/delete', TrainerDeleteView.as_view(), name='trainer-delete')
 ]
--- cardquest/views.py
+++ cardquest/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from .models import PokemonCard, Trainer
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
+from .models import PokemonCard, Trainer, Collection
+from .forms import TrainerForm, PokemonCardForm
+from django.urls import reverse_lazy
 
 
 class HomePageView(ListView):
@@ -17,4 +20,21 @@ class TrainerList(ListView):
     context_object_name = 'trainer'
     template_name = 'trainers.html'
     paginate_by = 15
+
+class TrainerCreateView(CreateView):
+    model = Trainer
+    form_class = TrainerForm
+    template_name = 'trainer_add.html'
+    success_url = reverse_lazy('trainer_list')
+
+class TrainerUpdateView(UpdateView):
+    model = Trainer
+    form_class = TrainerForm
+    template_name = 'trainer_edit.html'
+    success_url = reverse_lazy('trainer_list')
+
+class TrainerDeleteView(DeleteView):
+    model = Trainer
+    template_name = 'trainer_del.html'
+    success_url = reverse_lazy('trainer_list')
     
