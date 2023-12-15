4d1ac00b0e1fab139b14e3fe40c589af9410af76
crishelpc <202280108@psu.palawan.edu.ph>
2023-12-15 11:53:42 +0800

    update

--- cardquest/forms.py
+++ cardquest/forms.py
@@ -12,6 +12,14 @@ class TrainerForm(ModelForm):
         
         
 class PokemonCardForm(ModelForm):
+    release_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
     class Meta:
         model = PokemonCard
         fields = '__all__'
+
+class CollectionForm(ModelForm):
+    collection_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+
+    class Meta:
+        model = Collection
+        fields = "__all__"
--- cardquest/urls.py
+++ cardquest/urls.py
@@ -1,13 +1,20 @@
 from django.urls import path
-from .views import HomePageView, TrainerList, TrainerCreateView, TrainerUpdateView, TrainerDeleteView
 from cardquest import views
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
-    path('trainer_list', TrainerList.as_view(), name='trainer_list'),
-    path('pokemon-card', TrainerList.as_view(), name='pokemon-card'),
-    path('collection', TrainerList.as_view(), name='collection'),
-    path('trainer_list/add', TrainerCreateView.as_view(), name='trainer-add'), 
-    path('trainer_list/<pk>', TrainerUpdateView.as_view(), name='trainer-update'),
-    path('trainer_list/<pk>/delete', TrainerDeleteView.as_view(), name='trainer-delete')
+    path('', views.HomePageView.as_view(), name='home'),
+    path('trainer_list', views.TrainerList.as_view(), name='trainer-list'),
+    path('trainer_list/add', views.TrainerCreateView.as_view(), name='trainer-add'), 
+    path('trainer_list/<pk>', views.TrainerUpdateView.as_view(), name='trainer-update'),
+    path('trainer_list/<pk>/delete', views.TrainerDeleteView.as_view(), name='trainer-delete'), 
+  
+    path('pokemon_card', views.PokemonCardListView.as_view(), name='pokemon-card'),
+    path('pokemon_card/add', views.PokemonCardCreateView.as_view(), name='pokemon-card-add'),
+    path('pokemon_card/<pk>', views.PokemonCardUpdateView.as_view(), name='pokemon-card-update'), 
+    path('pokemon_card/<pk>/del', views.PokemonCardDeleteView.as_view(), name='pokemon-card-delete'),
+
+    path('collection', views.CollectionList.as_view(), name='collection'),
+    path('collection/add', views.CollectionCreateView.as_view(), name='collection-add'),
+    path('collection/<pk>', views.CollectionUpdateView.as_view(), name='collection-update'), 
+    path('collection/<pk>/del', views.CollectionDeleteView.as_view(), name='collection-delete'),
 ]
